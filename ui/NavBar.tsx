d11164aad2a9635b2cf664d3a4d42cfeffb06c7e
Leiko Lei <chachafrdark@gmail.com>
2022-12-14 23:16:31 +0100

    fixed navbar

--- ui/NavBar.tsx
+++ ui/NavBar.tsx
@@ -4,19 +4,23 @@ import React from 'react';
 export default function NavBar() {
   return (
     <>
-      <nav className="flex justify-center bg-secondary-800">
-        <div className="w-7xl flex px-4">
-          <div className="flex gap-3 py-4">
+      <nav className="flex justify-center bg-secondary-800 text-white">
+        <div className="mx-auto flex min-w-[1440px] px-4">
+          <div className="flex w-full items-center justify-between py-4">
             <Link href="/">
               <img
                 style={{ imageRendering: 'crisp-edges' }}
                 src="/favicon.ico"
               ></img>
             </Link>
-
-            <Link href="/posts" className="ml-9 p-2 text-xl">
-              works
-            </Link>
+            <div className="flex items-center gap-3">
+              <Link href="/posts" className="ml-9 p-2 text-xl">
+                works
+              </Link>
+              <Link href="/about" className="ml-9 p-2 text-xl">
+                about
+              </Link>
+            </div>
           </div>
         </div>
       </nav>
