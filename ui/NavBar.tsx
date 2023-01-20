bf7abfd8d05b36bddf0e1a791be33b686bb8b1cf
Léo Paillé <leo.paille@etu.u-bordeaux.fr>
2023-01-20 09:50:06 +0100

    fix: :lipstick: responsive navbar text size

--- ui/NavBar.tsx
+++ ui/NavBar.tsx
@@ -6,14 +6,15 @@ export default function NavBar() {
     <>
       <nav className="bg-secondary-400 text-white dark:bg-[#111]">
         <div className="w-container e-container">
-          <div className="flex w-full items-center justify-between py-[15px]">
+          <div className="flex w-full items-center justify-between py-8 lg:py-[15px]">
             <Link href="/">
               <img
-                style={{ imageRendering: 'crisp-edges', height: '50px' }}
+                className="h-20 lg:h-14"
+                style={{ imageRendering: 'crisp-edges' }}
                 src="/favicon.ico"
               ></img>
             </Link>
-            <div className="flex items-center gap-3 font-bold">
+            <div className="flex items-center gap-3 text-4xl font-bold lg:text-base">
               <Link href="/posts" className="ml-9 p-2 small-caps">
                 blog
               </Link>
