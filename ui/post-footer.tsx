d0225e2def11e407ecb976dcd996f6aa736eb02a
Léo Paillé <leo.paille@etu.u-bordeaux.fr>
2023-01-20 09:45:10 +0100

    fix: :lipstick: post responsive text-size

--- ui/post-footer.tsx
+++ ui/post-footer.tsx
@@ -14,15 +14,17 @@ const PostFooter = ({ title, coverImage, date, authors }: Props) => {
   return (
     <div className="e-container w-container mx-auto mt-20 flex-col">
       <div className="border-t-[1px] border-[#e5e8ed] py-4">
-        <h3 className="pb-2 text-2xs text-gray-400">Authors</h3>
-        <p className="text-[16px] text-secondary-600">{authors}</p>
+        <h3 className="pb-2 text-2xl text-gray-400 lg:text-2xs">Authors</h3>
+        <p className="text-3xl text-secondary-600 dark:text-white lg:text-[16px]">
+          {authors}
+        </p>
       </div>
 
       <div className="border-t-[1px] border-[#e5e8ed] py-4">
-        <h3 className="pb-2 text-2xs text-gray-400">Published</h3>
+        <h3 className="pb-2 text-2xl text-gray-400 lg:text-2xs">Published</h3>
         <DateFormatter
           dateString={date}
-          className="text-[16px] text-secondary-600"
+          className="text-3xl text-secondary-600 dark:text-white lg:text-[16px]"
         />
       </div>
     </div>
