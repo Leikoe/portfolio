7fc5d2d905f99fba2a9b016f46602c54acad57d0
Léo Paillé <leo.paille@etu.u-bordeaux.fr>
2023-01-20 10:44:07 +0100

    fix: :lipstick: /posts y padding fix

--- ui/more-stories.tsx
+++ ui/more-stories.tsx
@@ -17,7 +17,7 @@ const MoreStories = ({ name, posts }: Props) => {
       {/* <h2 className="mb-8 text-5xl font-bold leading-tight tracking-tighter md:text-7xl">
         {name}
       </h2> */}
-      <div className="w-container e-container flex flex-col gap-1 pt-12">
+      <div className="w-container e-container flex flex-col gap-1 py-12">
         {posts.map((post) => (
           <>
             <motion.a
